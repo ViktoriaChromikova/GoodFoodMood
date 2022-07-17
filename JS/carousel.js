0240291ccbbb3bd19170a0b1c89b4a61eb89d6c5
ViktoriaChromikova <83373204+ViktoriaChromikova@users.noreply.github.com>
2022-07-17 22:55:11 +0200

    start

---
+++ JS/carousel.js
@@ -0,0 +1,37 @@
+document.querySelectorAll(".foods").forEach((carousel) => {
+    const items = carousel.querySelectorAll(".links");
+    const buttonsHtml = Array.from(items, () => {
+      return `<span class="foods__button"></span>`;
+    });
+  
+    carousel.insertAdjacentHTML(
+      "beforeend",
+      `
+          <div class="foods__nav">
+              ${buttonsHtml.join("")}
+          </div>
+      `
+    );
+  
+    const buttons = carousel.querySelectorAll(".foods__button");
+  
+    buttons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+
+        items.forEach((item) =>
+          item.classList.remove("links--selected")
+        );
+        buttons.forEach((button) =>
+          button.classList.remove("foods__button--selected")
+        );
+  
+        items[i].classList.add("links--selected");
+        button.classList.add("foods__button--selected");
+      });
+    });
+  
+  
+    items[0].classList.add("links--selected");
+    buttons[0].classList.add("foods__button--selected");
+  });
+  
